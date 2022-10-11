4c751d73db45b4be789e06ccc1e933166ded9c44
Saad Malik <saad_malik200@hotmail.com>
2022-10-11 13:52:22 +0200

    ex5

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,24 @@
-import React from "react"
-const Alert = props => {
-	//add the condition inside this function
+import React from "react";
+const Alert = (props) => {
+  //add the condition inside this function
+  if (!props.show) {
+    return null;
+  } else {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is a primary alert-check it out!{" "}
+      </div>
+    );
+  }
 
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+  //   return (
+  //     props.show && (
+  //       <div className="alert alert-danger" role="alert">
+  //         This is a primary alert-check it out!
+  //       </div>
+  //     )
+
+  //   );
 };
 
 export default Alert;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Alert from "./Alert"
+import React from "react";
+import Alert from "./Alert";
 export default function App() {
-    return (
-        <div>
-		<Alert show={true} text="This is a primary alert-check it out!" />
-		<Alert show={false} text="This is a primary alert-check it out!" />
-	</div>
-    )
+  return (
+    <div>
+      <Alert show={true} text="This is a primary alert-check it out!" />
+      <Alert show={false} text="This is a primary alert-check it out!" />
+    </div>
+  );
 }
